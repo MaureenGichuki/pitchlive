857ae7a75b0373e1c779193a62c137de841dde54
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-05-11 16:41:10 +0300

    "Feat: main files"

---
+++ app/main/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,ValidationError
+from wtforms.validators import DataRequired,Email
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write down your bio...',validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class PitchForm(FlaskForm):
+    title=StringField('Pitch title',validators=[DataRequired()])
+    pitch=TextAreaField('Pitch description.',validators = [DataRequired()])
+    submit = SubmitField('Post')
+
+class CommentForm(FlaskForm):
+    comment=TextAreaField('Pitch comment.',validators = [DataRequired()])
+    submit = SubmitField('Submit')

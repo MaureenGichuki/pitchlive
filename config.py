b78d9766c891db0575bccae58f49be62a8f39357
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-05-11 16:38:04 +0300

    "Feat: configuration file"

---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:mimo1234@localhost/maureen'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #email configurations
+
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USE_SSL= True
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:mimo1234@localhost/maureen'
+
+
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:mimo1234@localhost/maureen'
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+
+}

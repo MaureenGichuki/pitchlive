ca5e525cb209c3c5abaac7fb48acb646072583a1
MaureenGichuki <maureen.gichuki@student.moringaschool.com>
2022-05-11 17:19:55 +0300

    "Fix: bug fix"

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_USERNAME = 'maureenwaruguru@gmail.com'
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     MAIL_USE_SSL= True
 
